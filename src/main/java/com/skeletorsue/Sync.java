e1a986167cd36c43d07d0758b72b4a9d120749dd
Chris Staley <skeletorsue@gmail.com>
2015-12-24 15:12:54 -0700

    Changing spaces to tabs.

--- src/main/java/com/skeletorsue/Sync.java
+++ src/main/java/com/skeletorsue/Sync.java
@@ -2,25 +2,25 @@ package com.skeletorsue;
 
 public class Sync {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        try {
-            Config config = LoadConfig(args);
-            System.out.printf("%n");
+		try {
+			Config config = LoadConfig(args);
+			System.out.printf("%n");
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
 
-    public static Config LoadConfig(String[] args) throws Exception {
-        Stopwatch timer = new Stopwatch();
-        System.out.print("Loading config.ini - READING\r");
-        Config config = new Config(args);
-        System.out.print("Loading config.ini - LOADED " + timer.RunTime(3) + "s\r\n");
+	public static Config LoadConfig(String[] args) throws Exception {
+		Stopwatch timer = new Stopwatch();
+		System.out.print("Loading config.ini - READING\r");
+		Config config = new Config(args);
+		System.out.print("Loading config.ini - LOADED " + timer.RunTime(3) + "s\r\n");
 
-        return config;
-    }
+		return config;
+	}
 
 }
